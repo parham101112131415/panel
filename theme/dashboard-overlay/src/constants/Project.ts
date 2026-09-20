33c1331b6c704885fed3eaeb1e3437e355942bfa
parham101112131415 <parham101112131415@users.noreply.github.com>
2026-09-20 12:08:19 +0330

    Revert theme changes, restore original deploy

--- theme/dashboard-overlay/src/constants/Project.ts
+++
@@ -1,3 +0,0 @@
-export const REPO_URL = 'https://github.com/PasarGuard/panel'
-export const ORGANIZATION_URL = 'https://github.com/PasarGuard'
-export const DOCUMENTATION = 'https://docs.pasarguard.org'
